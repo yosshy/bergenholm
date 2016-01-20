1cd2da4e2987ab002c33267c4e16d623c0710e8e
Yuuichi Fujioka <fujioka.yuuichi@gmail.com>
2016-01-20 11:51:50 +0900

    fix initialize flow

--- bergenholm/__init__.py
+++ bergenholm/__init__.py
@@ -10,12 +10,18 @@ from bergenholm.views.groups import groups
 from bergenholm.views.templates import templates
 from bergenholm.views.power import power
 
-app = Flask(__name__)
-database.init_db(app)
 
-app.register_blueprint(frontend, url_prefix='')
-app.register_blueprint(ipxe, url_prefix='/ipxe')
-app.register_blueprint(hosts, url_prefix='/api/1.0/hosts')
-app.register_blueprint(groups, url_prefix='/api/1.0/groups')
-app.register_blueprint(templates, url_prefix='/api/1.0/templates')
-app.register_blueprint(power, url_prefix='/api/1.0/power')
+def setup_app(config_obj):
+
+    app = Flask(__name__)
+    app.config.from_object(config_obj)
+
+    database.init_db(app)
+
+    app.register_blueprint(frontend, url_prefix='')
+    app.register_blueprint(ipxe, url_prefix='/ipxe')
+    app.register_blueprint(hosts, url_prefix='/api/1.0/hosts')
+    app.register_blueprint(groups, url_prefix='/api/1.0/groups')
+    app.register_blueprint(templates, url_prefix='/api/1.0/templates')
+    app.register_blueprint(power, url_prefix='/api/1.0/power')
+    return app
